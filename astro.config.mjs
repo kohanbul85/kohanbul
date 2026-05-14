30f45b21af79b5fe14911796417f2e6fcd08f815
kohanbul85 <kohanbul85@gmail.com>
2026-05-14 16:15:06 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://jsm000.github.io',
+  site: 'https://www.kohanbul.org',
   base: '/',
   integrations: [sitemap()],
   output: 'static',
